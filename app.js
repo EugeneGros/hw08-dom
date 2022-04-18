ca14f01fe9ee5c856fe0314ce71484c638d4598c
Ievgen <eugenegros.eg@gmail.com>
2022-04-18 15:44:07 +0300

    event-13

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // const noteE = document.getElementById("text").value;
 const noteE = document.getElementById("text");
-const btnE = document.querySelector("#btn");
+const btnE = document.getElementById("btn");
 const olToDoListE = document.getElementById("list");
 
 btnE.addEventListener("click", onClick);
@@ -8,9 +8,21 @@ btnE.addEventListener("click", onClick);
 function onClick() {
   const note = noteE.value.trim();
   if (note) {
-    const liE = document.createElement("li");
-    liE.textContent = note;
-    noteE.value = "";
-    olToDoListE.append(liE);
+    makeLi();
   }
+}
+
+noteE.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("btn").click();
+  }
+});
+
+function makeLi() {
+  const liE = document.createElement("li");
+  liE.textContent = noteE.value.trim();
+  noteE.value = "";
+  olToDoListE.append(liE);
+  noteE.focus();
 }
