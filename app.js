506a1587585ad5910c7ff5ee8365d0acf0ac64fe
Ievgen <eugenegros.eg@gmail.com>
2022-04-19 16:52:21 +0300

    hw-09 ver.01

--- app.js
+++ app.js
@@ -1,28 +1,73 @@
 // const noteE = document.getElementById("text").value;
-const noteE = document.getElementById("text");
+const inpLastEl = document.getElementById("inpL");
+const inpFirstEl = document.getElementById("inpF");
+const inpPhomeEl = document.getElementById("inpP");
 const btnE = document.getElementById("btn");
 const olToDoListE = document.getElementById("list");
 
-btnE.addEventListener("click", onClick);
+btnE.addEventListener("click", onAddList);
 
-function onClick() {
-  const note = noteE.value.trim();
-  if (note) {
-    makeLi();
+const templateE = document.getElementById("template");
+
+function onAddList(params) {
+  if (!chackValue()) {
+    return;
   }
+
+  const note = `${inpLastEl.value} ${inpFirstEl.value} ${inpPhomeEl.value}`;
+  const el = createNoteE(note);
+  addElement(el, olToDoListE);
+  clearValue(inpLastEl);
+  clearValue(inpFirstEl);
+  clearValue(inpPhomeEl);
 }
 
-noteE.addEventListener("keyup", function (event) {
+inpLastEl.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.getElementById("btn").click();
+    onAddList();
   }
 });
 
-function makeLi() {
-  const liE = document.createElement("li");
-  liE.textContent = noteE.value.trim();
-  noteE.value = "";
-  olToDoListE.append(liE);
-  noteE.focus();
+inpFirstEl.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    onAddList();
+  }
+});
+
+inpPhomeEl.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    onAddList();
+  }
+});
+
+function createNoteE(note) {
+  const el = templateE.innerHTML.replace("{{note}}", note);
+  return el;
+}
+
+function addElement(element, container) {
+  container.innerHTML += element;
+}
+
+function clearValue(inpEl) {
+  inpEl.value = "";
+}
+
+function chackValue() {
+  if (!inpLastEl.value.trim()) {
+    alert("LastName is Empty");
+    return;
+  }
+  if (!inpFirstEl.value.trim()) {
+    alert("FirstName is Empty");
+    return;
+  }
+  if (!inpPhomeEl.value.trim()) {
+    alert("PhoneNumber is Empty");
+    return;
+  }
+  return true;
 }
