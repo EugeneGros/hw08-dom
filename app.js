d4c5971e58cab9c5d961d6fc76833b4165d80030
Ievgen <eugenegros.eg@gmail.com>
2022-04-26 13:51:08 +0300

    new

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const inpFirstEl = document.getElementById("inpF");
 const inpPhomeEl = document.getElementById("inpP");
 const btnE = document.getElementById("btn");
 const olToDoListE = document.getElementById("list");
+const btnContainerControl = document.getElementById("btn-container");
+const inputsE = document.querySelectorAll(".user-info");
 
 btnE.addEventListener("click", onAddList);
 
 const templateE = document.getElementById("template");
 
 function onAddList(params) {
-  if (!chackValue()) {
+  // if (!chackValue()) {
+  //   return;
+  // }
+  if (dataIsNotValid(inputsE)) {
+    alert("Empty field");
     return;
   }
 
@@ -22,21 +28,7 @@ function onAddList(params) {
   clearValue(inpPhomeEl);
 }
 
-inpLastEl.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    onAddList();
-  }
-});
-
-inpFirstEl.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    onAddList();
-  }
-});
-
-inpPhomeEl.addEventListener("keyup", function (event) {
+btnContainerControl.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     onAddList();
@@ -56,18 +48,22 @@ function clearValue(inpEl) {
   inpEl.value = "";
 }
 
-function chackValue() {
-  if (!inpLastEl.value.trim()) {
-    alert("LastName is Empty");
-    return;
-  }
-  if (!inpFirstEl.value.trim()) {
-    alert("FirstName is Empty");
-    return;
-  }
-  if (!inpPhomeEl.value.trim()) {
-    alert("PhoneNumber is Empty");
-    return;
-  }
-  return true;
+// function chackValue() {
+//   if (!inpLastEl.value.trim()) {
+//     alert("LastName is Empty");
+//     return;
+//   }
+//   if (!inpFirstEl.value.trim()) {
+//     alert("FirstName is Empty");
+//     return;
+//   }
+//   if (!inpPhomeEl.value.trim()) {
+//     alert("PhoneNumber is Empty");
+//     return;
+//   }
+//   return true;
+// }
+
+function dataIsNotValid(elements) {
+  return [...elements].some((e) => !e.value.trim());
 }
